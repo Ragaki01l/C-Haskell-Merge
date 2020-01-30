643339acd48d0ea1a0667b155e1ecc6ea8dd6cb1
William Jorgensen <willwjorgensen@yahoo.com>
2020-01-29 19:06:11 -0800

    Finished msort scaffolding

--- C-Mege.c
+++ C-Mege.c
@@ -1,33 +1,70 @@
+//#include <stdio.h>
+
+#define max 10
+
+int arr[] = {4, 65, 2, -31, 0, 99, 2 ,83, 782, 1};
+
 int main()
 {
-    int arr[] = {4, 65, 2, -31, 0, 99, 2 ,83, 782, 1};
     int arrsize = sizeof(arr);
+    int arrpoint = &arr;
+
+    msort(arrpoint, arrsize);
 }
 
-void msort(init *a, int n)
+void msort(int *a, int n)
 {
     int low,mid;
 
-    low= 0;
+    low = 0;
     mid = (low + n)/2;
 
-    if(low < n)
-    {
-        msort(low,mid); //recursion on left side of tree
-        msort(mid + 1, n); //recursion on right side of tree
-        arraymerge(low, mid, n, *a);
-    }
+    mergeS(*a, low, n);
 }
 
-void arraymerge(int low, int mid, int high, int *a)
+
+void mergeS(int arr[], int low, int high)
 {
-    int temp[high- 1]; //initialize temp array
+    int mid = (low + high)/2;
 
-    int mid2 = mid + 1;
-    int = low;
-
-    for(low, mid2, i; low <= mid && mid2 <= high, i++)
+    if(low < high)
     {
-        
+        mergeS(arr, low, mid);
+        mergeS(arr, mid + 1, high);
+        arraymerge( low, mid, high, arr);
+    }
+
+}
+
+
+void arraymerge(int low, int mid, int high, int arr[])
+{
+    int temp[max]; //initialize temp array
+
+    int i = low; //start of 1st array
+    int mid2 = mid + 1; //start of 2nd array
+    int start = 0;
+
+    while(i <= mid && mid2 <= high) //Sorts array and puts them into respective lists
+    {
+        if(arr[i] < arr[mid2])
+            temp[start++] = arr[i++];
+        else
+            temp[start++] = arr[mid2++];
+    }
+
+    while(i <= mid) //Gets remaining numbers from 1st array
+    {
+        temp[start++] = arr[i++];
+    }
+
+    while (mid2 <= high) //Gets remaining numbers from 2nd array
+    {
+        temp[start++] = arr[mid2++]
+    }
+
+    for(i = low, mid2 = 0; i <= high, i++, j++) //Puts numbers back into array
+    {
+        arr[i] = temp[j];
     }
 }
