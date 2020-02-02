e995a195c683c2a5bde581dc4650d1ba7b4750b9
William <willwjorgensen@yahoo.com>
2020-02-02 02:21:51 -0800

    Finished Merge Sort in C

--- C-Merge.c
+++ C-Merge.c
@@ -1,56 +1,30 @@
 #include <stdio.h>
+#define max 9
 
-#define max 10
-
-int arr[] = {4, 65, 2, -31, 0, 99, 2 ,83, 782, 1};
-
-int main()
-{
-    int arrsize = sizeof(arr);
-    int arrpoint = &arr;
-
-    msort(arrpoint, arrsize);
-}
-
-void msort(int *a, int n)
-{
-    int low,mid;
-
-    low = 0;
-    mid = (low + n)/2;
-
-    mergeS(*a, low, n);
-}
+int arr[] = {4, 65, 2, -31, 0, 99, 2 ,83, 782, 1}; //Initialize array to be sorted
 
 
-void mergeS(int arr[], int low, int high)
-{
-    int mid = (low + high)/2;
+//Initialize all functions to be used
+void mergeS(int *arr, int low, int high);
+void arraymerge(int low, int mid, int mid2 ,int high, int arr[]);
+void msort(int *a, int n);
 
-    if(low < high)
-    {
-        mergeS(arr, low, mid);
-        mergeS(arr, mid + 1, high);
-        arraymerge( low, mid, high, arr);
-    }
-
-}
-
-
-void arraymerge(int low, int mid, int high, int arr[])
+void arraymerge(int low, int mid, int mid2, int high, int arr[])
 {
     int temp[max]; //initialize temp array
 
     int i = low; //start of 1st array
-    int mid2 = mid + 1; //start of 2nd array
+    int i2 = mid2; //start of 2nd array
     int start = 0;
 
-    while(i <= mid && mid2 <= high) //Sorts array and puts them into respective lists
+    while(i <= mid && i2 <= high) //Sorts array and puts them into respective lists
     {
-        if(arr[i] < arr[mid2])
+        if(arr[i] < arr[i2])
+           {
             temp[start++] = arr[i++];
+           }
         else
-            temp[start++] = arr[mid2++];
+            temp[start++] = arr[i2++];
     }
 
     while(i <= mid) //Gets remaining numbers from 1st array
@@ -58,13 +32,58 @@ void arraymerge(int low, int mid, int high, int arr[])
         temp[start++] = arr[i++];
     }
 
-    while (mid2 <= high) //Gets remaining numbers from 2nd array
+    while (i2 <= high) //Gets remaining numbers from 2nd array
     {
-        temp[start++] = arr[mid2++];
+        temp[start++] = arr[i2++];
     }
 
-    for(i = low, mid2 = 0; i <= high; i++, mid2++) //Puts numbers back into array
+    for(int i = low, i2 = 0; i <= high; i++, i2++) //Puts numbers back into original array
     {
-        arr[i] = temp[mid2++];
+        arr[i] = temp[i2];
+        
+    }
+}
+
+
+void mergeS(int arr[], int low, int high)
+{
+    int mid;
+
+    if(low < high)
+    {
+        mid = (low + high)/2;
+        mergeS(arr, low, mid);
+        mergeS(arr, mid + 1, high);
+        arraymerge( low, mid, mid + 1, high, arr);
+    }
+
+}
+
+void msort(int *a, int n)
+{
+    int low;
+    low = 0;
+    mergeS(a, low, n - 1);
+}
+
+int main()
+{
+    int arrsize = sizeof(arr);
+    int *arrpoint;
+
+    arrpoint = arr;
+
+    printf("Unsorted array is : ");
+
+    for(int i = 0; i < max; i++){
+        printf("%d : ", arr[i]);
+    }
+
+    msort(arr, max);
+
+    printf("\n Sorted array is : ");
+
+    for(int j = 0; j < max; j++){
+        printf("%d : ", arr[j]);
     }
 }
