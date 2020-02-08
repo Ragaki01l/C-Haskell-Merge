5cb338d51cccf79ee2749efdb6a4ce4d00dfe7f8
William <willwjorgensen@yahoo.com>
2020-02-07 22:43:04 -0800

    Haskell Merge Sort scaffolding

---
+++ HaskellMergeSort.hs
@@ -0,0 +1,7 @@
+--HOW TO RUN Haskell
+-- 1) New Terminal 
+-- 2) Type "ghci" in terminal
+-- 3) Load Haskell file by :l HaskellFile.hs
+
+merge :: [Integer] -> [Integer] -- Sets up the expected inputs to be used
+merge []   = []  -- Empty Set Rule
