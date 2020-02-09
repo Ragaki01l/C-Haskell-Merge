5ae7d2783780ef6e57cfbb95816ba127e7d58a95
William <willwjorgensen@yahoo.com>
2020-02-08 23:32:15 -0800

    Implemented mergesort in haskell

--- HaskellMergeSort.hs
+++ HaskellMergeSort.hs
@@ -3,5 +3,22 @@
 -- 2) Type "ghci" in terminal
 -- 3) Load Haskell file by :l HaskellFile.hs
 
-merge :: [Integer] -> [Integer] -- Sets up the expected inputs to be used
-merge []   = []  -- Empty Set Rule
+
+merge :: [Integer] -> [Integer] -> [Integer] -- Sets up the expected inputs to be used
+merge xs [] = xs  -- Empty Set Rule
+merge [] xs = xs
+merge (x: xs) (y:ys) | x < y  = x:(merge xs (y:ys))
+                     |otherwise = y:(merge (x:xs) ys)
+
+divide :: [Integer] -> ([Integer], [Integer])
+divide [] = ([], [])
+divide [x] = ([x], [])
+divide (x:y:xs) = ((x:ys), (y:zs))
+    where (ys, zs) = divide xs
+
+
+mergesort :: [Integer] -> [Integer]
+mergesort [] = []
+mergesort [x] = [x]
+mergesort xs = merge (mergesort ys) (mergesort zs)
+    where (ys, zs) = divide xs
